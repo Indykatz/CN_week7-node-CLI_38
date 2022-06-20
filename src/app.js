376f2fcaa2c3385503e55caefb143314fef64597
Indykatz <cats83@me.com>
2022-06-20 17:10:45 +0100

    yargs

--- src/app.js
+++ src/app.js
@@ -1,7 +1,42 @@
 // looks for .js file
 const Movie = require("./utils");
+// import yargs
+const yargs = require("yargs");
+console.log(yargs.argv);
 // input process
-const input = process.argv;
+// const input = process.argv;
+
+// const TheApp = () => {
+//   if (yargs.argv.command === "add") {
+//     const movieObj = new Movie(yargs.argv.title, yargs.argv.actor);
+//     movieObj.add();
+//   }
+// };
+// TheApp(process.argv);
+
+const TheApp = (argvArray) => {
+  console.log(argvArray);
+  console.log(argvArray.length);
+  for (i = 2; i < argvArray.length; i++) {
+    if (argvArray[i] === "--title" && argvArray[i + 2] === "--actor") {
+      const movieObj = new Movie(argvArray[i + 1], argvArray[i + 3]);
+      movieObj.add();
+      i += 3;
+    }
+  }
+};
+
+TheApp(process.argv);
+
+// Iterated through the input starting at index 2
+// for (i = 2; i < input.length; i++) {
+//   console.log(input[i]);
+//   if (input[i] === "add" && input[i + 1] != "add" && input[i + 2] != "add") {
+//     const movieObj = new Movie(input[i + 1], input[i + 2]);
+//     movieObj.add();
+//     i += 2;
+//   }
+// }
 
 // // Iterated through the input starting at index 2
 // for (i = 2; i < input.length; i++) {
@@ -20,13 +55,3 @@ const input = process.argv;
 //     movieObj.add();
 //   }
 // }
-
-// Iterated through the input starting at index 2
-for (i = 2; i < input.length; i++) {
-  console.log(input[i]);
-  if (input[i] === "add" && input[i + 1] != "add" && input[i + 2] != "add") {
-    const movieObj = new Movie(input[i + 1], input[i + 2]);
-    movieObj.add();
-    i += 2;
-  }
-}
