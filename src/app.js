3a9c886a202d536ea15fe49c2810b6ae3596f26b
Indykatz <cats83@me.com>
2022-06-20 11:34:24 +0100

    Comments added

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
 // looks for .js file
 const Movie = require("./utils");
+// input process
 const input = process.argv;
 
+// if statement on input
 if (input[2] === "add") {
   const movieObj = new Movie(input[3], input[4]);
   movieObj.add();
--- src/utils/index.js
+++ src/utils/index.js
@@ -1,9 +1,12 @@
+// New Movie Array
 const moviesArray = [];
+// Movie class
 class Movie {
   constructor(title, actor = "Not specified") {
     this.title = title;
     this.actor = actor;
   }
+  // Add movie function
   add() {
     moviesArray.push(this);
     console.log(moviesArray);
