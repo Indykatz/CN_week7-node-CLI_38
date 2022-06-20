2bd8e35c51a0ddbbf37e4a08a01d0c0763163b32
Indykatz <cats83@me.com>
2022-06-20 15:08:24 +0100

    working v3 with comments

--- src/app.js
+++ src/app.js
@@ -3,20 +3,30 @@ const Movie = require("./utils");
 // input process
 const input = process.argv;
 
+// // Iterated through the input starting at index 2
+// for (i = 2; i < input.length; i++) {
+//   // slices the input into sections of 3
+//   let slicedSection = input.slice(i, i + 3);
+//   // a console log to see the process
+//   console.log(slicedSection);
+//   // If index 0 in sliced section is "add" and add isnt a value in the sliced section
+//   if (
+//     slicedSection[0] === "add" &&
+//     slicedSection[1] != "add" &&
+//     slicedSection[2] != "add"
+//   ) {
+//     // add movie
+//     const movieObj = new Movie(slicedSection[1], slicedSection[2]);
+//     movieObj.add();
+//   }
+// }
+
 // Iterated through the input starting at index 2
 for (i = 2; i < input.length; i++) {
-  // slices the input into sections of 3
-  let slicedSection = input.slice(i, i + 3);
-  // a console log to see the process
-  console.log(slicedSection);
-  // If index 0 in sliced section is "add" and add isnt a value in the sliced section
-  if (
-    slicedSection[0] === "add" &&
-    slicedSection[1] != "add" &&
-    slicedSection[2] != "add"
-  ) {
-    // add movie
-    const movieObj = new Movie(slicedSection[1], slicedSection[2]);
+  console.log(input[i]);
+  if (input[i] === "add" && input[i + 1] != "add" && input[i + 2] != "add") {
+    const movieObj = new Movie(input[i + 1], input[i + 2]);
     movieObj.add();
+    i += 2;
   }
 }
