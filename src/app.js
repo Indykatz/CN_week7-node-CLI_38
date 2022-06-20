69e468dd8ad111b59d4b461deba59990f60c5438
Indykatz <cats83@me.com>
2022-06-20 14:51:32 +0100

    working v2 with comments

--- src/app.js
+++ src/app.js
@@ -3,14 +3,20 @@ const Movie = require("./utils");
 // input process
 const input = process.argv;
 
-// if statement on input
+// Iterated through the input starting at index 2
 for (i = 2; i < input.length; i++) {
+  // slices the input into sections of 3
   let slicedSection = input.slice(i, i + 3);
+  // a console log to see the process
   console.log(slicedSection);
-  if (slicedSection[0] === "add") {
-    if (slicedSection[1] != "add" && slicedSection[2] != "add") {
-      const movieObj = new Movie(slicedSection[1], slicedSection[2]);
-      movieObj.add();
-    }
+  // If index 0 in sliced section is "add" and add isnt a value in the sliced section
+  if (
+    slicedSection[0] === "add" &&
+    slicedSection[1] != "add" &&
+    slicedSection[2] != "add"
+  ) {
+    // add movie
+    const movieObj = new Movie(slicedSection[1], slicedSection[2]);
+    movieObj.add();
   }
 }
